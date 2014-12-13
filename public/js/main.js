655e17015717fb1885cb6f2a0424752d441c1bcd
dkawashi <dkawashi@yahoo-corp.co.jp>
2014-12-13 17:23:57 +0900

    json 取得

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var hoge = 10;
 $.ajax({
-    url: 'json/iconti_medals.json',
+    url: 'json/conti_medals.json',
     success: function(data) {
         console.log(data);
         //$('body').append(data + "<br />");
