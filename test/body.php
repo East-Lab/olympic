5af475eee415b91f303f4a1fecd4827256869de6
kzhrknt <kuzuhara.1989@gmail.com>
2014-12-15 23:12:59 +0900

    オリンピックリング描写部分を正しく描いた

--- test/body.php
+++ test/body.php
@@ -1,17 +1,13 @@
 <div id="wrapper">
-
   <div id="rings">
-    <p>rings</p>
-    <div class="blue"></div>
+   <div class="blue"></div>
     <div class="yellow"></div>
     <div class="black"></div>
     <div class="green"></div>
     <div class="red"></div>
   </div>
-
   <div id="compe">
     <div class="year"></div>
     <div class="place"></div>
   </div>
-
 </div>
--- test/js/main.js
+++ test/js/main.js
@@ -1,13 +1,17 @@
 //ColorScheme
 var colorScheme = {
-  blue  : '#0386D0',
-  yellow: '#FDB232',
-  black : '#020202',
-  green : '#00A751',
-  red   : '#EE344E'
+  //olympic color
+  blue  : '#18B1DB',
+  yellow: '#F9D222',
+  black : '#231F20',
+  green : '#259F49',
+  red   : '#EE2724',
+  //others
+  bgc   : '#F5F2F2',
+  text  : '#231F20',
+  stext : '#E5E5E5',
 };
 
-// function
 $(function(){
   //読み込み完了後の処理
 
@@ -18,29 +22,17 @@ $(function(){
     'height'    : '68px',
     '-webiit-border-radius': '50%',
     'border-radius'        : '50%',
+    'overflow'  : 'visible',
     //set position
     'position'  : 'absolute',
-    'top'       : '50%',
+    'top'       : '196px',
     'left'      : '50%',
+    'margin-left': '-130px',
     'float'     : 'left',
     '-webkit-transform'    : 'scale(1)',
     '-webkit-transform'    : 'translate(0px,0px)',
   });
 
-/**
-  $('.yellow').css({
-    'border'    : 'solid 6px' + colorScheme['yellow'],
-    'width'     : '68px',
-    'height'    : '68px',
-    'position'  : 'absolute',
-    'margin'    : '44px',
-    'float'     : 'left',
-    '-webiit-border-radius': '50%',
-    'border-radius'        : '50%',
-    '-webkit-transform'    : 'scale(5.0)',
-    '-webkit-transform'    : 'translate(44px,34px)',
-  })
-**/
 
   $('.yellow').css({
     //draw circle
@@ -49,51 +41,65 @@ $(function(){
     'height'    : '68px',
     '-webiit-border-radius': '50%',
     'border-radius'        : '50%',
+    'overflow'  : 'visible',
     //set position
     'position'  : 'absolute',
-    'top'       : '50%',
+    'top'       : '236px',
     'left'      : '50%',
+    'margin-left': '-84px',
     'float'     : 'left',
     '-webkit-transform'    : 'scale(1)',
-    '-webkit-transform'    : 'translate(44px,34px)',
+    //'-webkit-transform'    : 'translate(44px,34px)',
   });
 
   $('.black').css({
+    //draw circle
     'border'    : 'solid 6px' + colorScheme['black'],
     'width'     : '68px',
     'height'    : '68px',
-    'position'  : 'absolute',
-    'margin'    : '44px',
-    'margin'    : '44px',
-    'float'     : 'left',
     '-webiit-border-radius': '50%',
     'border-radius'        : '50%',
-    '-webkit-transform'    : 'translate(85px, 0px)'
+    'overflow'  : 'visible',
+    //set position
+    'position'  : 'absolute',
+    'top'       : '196px',
+    'left'      : '50%',
+    'margin-left': '-40px',
+    'float'     : 'left',
+    //'-webkit-transform'    : 'translate(85px, 0px)'
   })
 
   $('.green').css({
+    //draw circle
     'border'    : 'solid 6px' + colorScheme['green'],
     'width'     : '68px',
     'height'    : '68px',
-    'position'  : 'absolute',
-    'margin'    : '44px',
-    'float'     : 'left',
     '-webiit-border-radius': '50%',
     'border-radius'        : '50%',
-    '-webkit-transform'    : 'scale(1)',
-    '-webkit-transform'    : 'translate(129px,34px)'
+    'overflow'  : 'visible',
+    //set position
+    'position'  : 'absolute',
+    'top'       : '236px',
+    'left'      : '50%',
+    'margin-left': '4px',
+    'float'     : 'left',
+    //'-webkit-transform'    : 'translate(129px,34px)'
   })
 
   $('.red').css({
+    //draw circle
     'border'    : 'solid 6px' + colorScheme['red'],
     'width'     : '68px',
     'height'    : '68px',
-    'position'  : 'absolute',
-    'margin'    : '44px',
-    'float'     : 'left',
     '-webiit-border-radius': '50%',
     'border-radius'        : '50%',
-    '-webkit-transform'    : 'scale(1)',
-    '-webkit-transform'    : 'translate(170px,0px)'
+    'overflow'  : 'visible',
+    //set position
+    'position'  : 'absolute',
+    'top'       : '196px',
+    'left'      : '50%',
+    'margin-left': '50px',
+    'float'     : 'left',
+    //'-webkit-transform'    : 'translate(170px,0px)'
   })
 });
